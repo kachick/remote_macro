351236ac71cd4e30ba0650f36ba9528cbf3e0f1a
Kenichi Kamiya <kachick1@gmail.com>
2013-01-08 15:46:35 +0900

    Fix load order

--- lib/remotemacro.rb
+++ lib/remotemacro.rb
@@ -1,9 +1,10 @@
 # Copyight (c) 2013 Kenichi Kamiya
 
-require 'remotemacro/version'
-
 module RemoteMacro
 
   class Error < StandardError; end
 
 end
+
+require_relative 'remotemacro/version'
+require_relative 'remotemacro/sshoperation'
